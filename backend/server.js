092531f8bffe86c3cf3749155d0eacf7c92cbe9c
David Agam <davidght1@gmail.com>
2023-02-21 18:04:44 +0200

    new file:   backend/controllers/userController.js 	new file:   backend/middleWare/errorMiddleware.js 	new file:   backend/models/userModel.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/userRoute.js 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,30 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Routes Middleware
+app.use("/api/users", userRoute);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+//Error Middleware
+app.use(errorHandler);
 //Connect to DB and start server
+const PORT = process.env.PORT || 5000;
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URI)
